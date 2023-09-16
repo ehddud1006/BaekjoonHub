428d47013a5a9f30975a72c59298900b4801a603
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-09-17 02:17:46 +0900

    test

--- scripts/baekjoon/baekjoon.js
+++ scripts/baekjoon/baekjoon.js
@@ -24,19 +24,19 @@ console.log('hi',submitButton)
 // 버튼에 클릭 이벤트 리스너 추가
 submitButton.addEventListener("click", function() {
   // 여기에 클릭될 때 실행할 함수 내용을 작성
-  log('submit button clicked');
+  console.log('submit button clicked');
  startLoader(); 
 });
 
 
 
-function startLoader() {
-  loader = setInterval(async () => {
+async function startLoader() {
+
           stopLoader();
           const bojData = await findData();
           await beginUpload(bojData);
   
-  }, 2000);
+
 }
 
 function stopLoader() {
